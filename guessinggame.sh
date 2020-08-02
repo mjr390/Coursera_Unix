44ad2d0a0eab29f5d7003d087dc9c1b04e1af482
mjr390 <mjr117@yahoo.com>
2020-08-01 20:00:25 -0400

    created a function where the users guesses how many files are in the current directory

--- guessinggame.sh
+++ guessinggame.sh
@@ -1,1 +1,20 @@
-First commit
+#!/usr/bin/env bash
+# File: guessinggame.sh
+function GuessingGame {
+
+echo "How many files are in the current directory? "
+read fileguess
+
+actualcount=$(ls -1q | wc -l)
+
+while [[ $fileguess -ne $actualcount ]]
+do 
+    echo "Your guess is incorrect, please try again "
+    read fileguess
+done
+
+echo "Congratulations! You are correct!"
+
+}
+
+GuessingGame
